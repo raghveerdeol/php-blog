36960b310ecf7b43a7d7da9cee28672d03a3a4ce
raghveerdeol <deolraghveer5@gmail.com>
2024-10-26 16:11:33 +0200

    created welcome page

---
+++ welcome.php
@@ -0,0 +1,46 @@
+<?php 
+    session_start();
+    if ($_SESSION['loggedin'] === false) {
+        header("location: login.php");
+    }
+    require_once "./config.php";
+    $postsQuery =
+    "SELECT posts.image, posts.title, categories.* FROM`posts`
+    JOIN `categories` ON posts.category_id = categories.id";
+
+    $postsResult = $link->query($postsQuery);
+    mysqli_close($link);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+    <link rel="stylesheet" href="./style/indexStyle.css">
+</head>
+<body>
+<header>
+        <nav class="navbar">
+            <div>ICON</div>
+            <div class="actions">
+                <a href="./logout.php">Logout</a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="cards-container">
+            <?php if(mysqli_num_rows($postsResult) > 0) {?>
+                <?php while($row = mysqli_fetch_assoc($postsResult)) {?>
+                    <div class="card">
+                        <img src="<?php echo $row["image"] ?>" alt="<?php echo $row["title"] ?> name">
+                        <h2><?php echo $row["title"] ?></h2>
+                        <p><?php echo $row["name"] ?></p>
+                    </div>
+                <?php } ?>
+            <?php } ?>
+        </div>
+    </main>
+</body>
+</html>
